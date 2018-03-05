dfc70e3640f0ed975162f90f92196ebda8130ac1
alelievr <alelievr@student.42.fr>
2018-03-05 02:34:33 +0100

    Added stupid google integration to the bot

--- sources/slack_bot.rb
+++ sources/slack_bot.rb
@@ -1,5 +1,7 @@
 require 'slack-ruby-client'
 require 'awesome_print'
+require 'json'
+require 'http'
 require_relative 'user_database.rb'
 
 class SlackBot
@@ -17,7 +19,10 @@ class SlackBot
 
 		@client = Slack::RealTime::Client.new
 
-		@client.on :message do |data| onMessage(data) end
+		@client.on :message do |data|
+			onDirectMessage(data) if data.channel[0] == 'D'
+			onChannelMessage(data) if data.channel[0] == 'C'
+		end
 
 		@client.on :hello do
 			  puts "Successfully connected, welcome '#{@client.self.name}' to the '#{@client.team.name}' team at https://#{@client.team.domain}.slack.com."
@@ -31,7 +36,7 @@ class SlackBot
 			  puts 'Connection has been disconnected.'
 		end
 
-		@client.start!
+		Thread.new { @client.start! }
 
 	end
 
@@ -40,7 +45,7 @@ class SlackBot
 		@client.message channel: chan, text: text
 	end
 
-	def onMessage(data)
+	def onChannelMessage(data)
 		#data:
 		#"type" => "message",
 		#"channel" => "D9JE10EBG",
@@ -50,15 +55,46 @@ class SlackBot
 		#"source_team" => "T9B6RUSKT",
 		#"team" => "T9B6RUSKT"
 
+		if data.text =~ /.*cluster/i || data.text =~ /.*stalker.*/i
+			puts data.text
+			onDirectMessage(data) 
+		end
+	end
+
+	def onDirectMessage(data)
+		return unless data.subtype.nil?
+
+		ap data
 		case data.text
-		when 'bot hi' then
-			sendMessage(data.channel, "Hi <@#{data.user}>!")
-		when /.*cluster.*/i, /.*stalker.*/i then
-			case data.text
-			when /.*who.*connected.*/i then
-				sendMessage(data.channel, @ud.get_users().map{|u| "#{u[:login42]} @ #{u[:last_seat]}" if u[:connected]}.compact.join("\n"))
-			end
+		when /hi/i, /hey/i
+			sendMessage(data.channel, "Hi <@#{data.user}> !")
+		when /where.*you/i
+			sendMessage(data.channel, "In your back !")
+		when /who.*connected/i then
+			sendMessage(data.channel, @ud.get_users().map{ |u| "#{u[:login42]} @ #{u[:last_seat]}" if u[:connected] }.compact.join("\n"))
+		else
+			q = data.text.sub(/.*(stalker[s]?|cluster[s]?)\s*[\?\.,!:]/i, '')
+			sendMessage(data.channel, askGoogle(q))
 		end
 
 	end
+
+	def askGoogle(query)
+		return "Nope sorry" if query.strip.empty?
+		puts "query: #{query}"
+		results = JSON.parse HTTP.get('https://www.googleapis.com/customsearch/v1', params: {
+            q: query,
+            key: ENV['GOOGLE_API_KEY'],
+            cx: ENV['GOOGLE_CSE_ID']
+        })
+        result = results['items'].first if results['items']
+        if result
+          	message = result['title'] + "\n" + result['link']
+        else
+          	message = "Wait what ?"
+		end
+
+		return message
+	end
+
 end
